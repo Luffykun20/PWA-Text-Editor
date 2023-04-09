b99838d53365abe9089ed265e4061626a4573d11
cristhian bustamante <cristhian_georgia_20@hotmail.com>
2023-04-09 06:08:29 -0400

    logic & installation methods, scripts, plugins, loaders,asset catching done

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) =>{
+  console.log( 'PUT to the jate database')
+const jateDb = await openDB('jate', 1);
+const jt = jateDb.transaction ('jate', 'readwrite');
+const store = jt.objectStore('jate');
+const request = store.put({ id: 1, value: content });
+const result = await request;
+console.log('new data saved to the jate database', result.value);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the jate database');
+  const jateDb = await openDB('jate', 1);
+  const jt = jateDb.transaction('jate', 'readonly');
+  const store = jt.objectStore('jate');
+  const request = storage.get(1);
+  const result = await request;
+  result
+    ? console.log('data recovered from the jate database', result.value)
+    : console.log('data not found in the jate database');
+}
 
 initdb();
